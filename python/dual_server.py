d3caebc597f9170fb0500e5771c1f29d517114a4
Tim Verburg <tverburg@lely.com>
2023-01-20 10:18:09 +0100

    Fixed the configuration file not being compiled

--- python/dual_server.py
+++ python/dual_server.py
@@ -3,7 +3,7 @@
 import rospy
 
 from dynamic_reconfigure.server import Server
-from franka_bimanual_teaching.cfg import dual_arm_teachingConfig
+from franka_bimanual_controllers.cfg import dual_arm_teachingConfig
 
 def callback(config, level):
     rospy.loginfo("""Reconfigure Request: {target_coupling_stiffness}, {execution_factor}, {panda_left_enable_correction}, {panda_right_enable_correction}, {dual_enable_correction}, {panda_left_feedback_factor_position}, {panda_left_feedback_factor_stiffness}, {panda_right_feedback_factor_position}, {panda_right_feedback_factor_stiffness}, {attractor_distance_threshold}, {trajectory_distance_threshold}, {correction_length_scale}, {correction_window_size}""".format(**config))
